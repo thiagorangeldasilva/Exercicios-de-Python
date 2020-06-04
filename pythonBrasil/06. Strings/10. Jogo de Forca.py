1743515a230c75f093cb0ddb599ce928d694620e
Thiago Rangel <thiagodasilva925@gmail.com>
2020-06-04 11:44:41 -0300

    Correção do Bug
    Corrigi o bug, e coloquei um banco pequeno de palavras

--- pythonBrasil/06. Strings/10. Jogo de Forca.py
+++ pythonBrasil/06. Strings/10. Jogo de Forca.py
@@ -14,15 +14,17 @@ A palavra é: _ E _ _ O
 Digite uma letra: S
 -> Você errou pela 2ª vez. Tente de novo!
 '''
+import random
+
 def converterPalavra(palavra):
     palavra = list(palavra)
     for indice in range(len(palavra)):
-        if palavra[indice] != ' ':
-            palavra[indice] = '_ '
+        if palavra[indice] != ' ':  
+            palavra[indice] = '_'
     return ''.join(palavra)
 
 def espaconaPalavra(palavra):
-    palavra = list(palavra)
+    palavra = list(palavra.upper())
     for indice in range(len(palavra)):
         if palavra[indice] != ' ':
             palavra[indice] = palavra[indice] + ' '
@@ -38,24 +40,64 @@ def letraescolhida(letra, palavra, novapalavra):
                 novapalavra[indice] = letra
     return ''.join(novapalavra)
 
-def printpalavra(novapalavra):
-    for indice in range(len(novapalavra)):
-        print(novapalavra[indice])
+def escolhertipoPalavra():
+    print('Escolha um tipo de palavra:')
+    print('1 - Fruta')
+    print('2 - Times de Fultebol')
+    print('3 - Nomes de Cidade')
 
-erro = 1
-palavra = 'Morango ovo'
-palavra = espaconaPalavra(palavra)
-novapalavra = converterPalavra(palavra)
-print(novapalavra, len(novapalavra))
-while erro <= 6:
+def tratarErroIntFloat(numero):
+	if numero != "":
+		if numero.isalpha() == False and numero.isalnum() == False:
+			return 1
+		elif numero.isdigit() == True:
+			return 2
+		else:
+			return 0
+	else:
+		return 0
+
+def trasnformar(tratar, numero):
+	if tratar == 1:
+		return float(numero)
+	elif tratar == 2:
+		return int(numero)
+cond = True
+erro = 0
+fruta = ['Morango', 'Banana', 'Tomate', 'Laranja', 'Abacate']
+times_de_futebol = ['Fluminense', 'Flamengo', 'Cruzeiro', 'Botafogo', 'Vasco', 'Internacional']
+cidades = ['Rio das Ostras', 'Rio de Janeiro', 'Minas Gerais', 'Fortaleza']
+palavra = [fruta, times_de_futebol, cidades]
+
+while cond:
+    escolhertipoPalavra()
+    opcao = input('Opcao escolhida: ')
+    if tratarErroIntFloat(opcao) == 1 or tratarErroIntFloat(opcao) == 2:
+        opcao = trasnformar(tratarErroIntFloat(opcao), opcao)
+        if opcao >= 1 and opcao <= 3:
+            break
+    print('Opção inválida.')
+
+palavraescolhida = espaconaPalavra(palavra[opcao - 1][random.randint(1, (len(palavra[opcao - 1]) - 1))])
+novapalavra = converterPalavra(palavraescolhida)
+
+print()
+print('A palavra é: ' + novapalavra)
+print()
+while cond:
     letra = input('Digite uma letra: ')
-    palavratemp = letraescolhida(letra, palavra, novapalavra)
-    print(novapalavra, len(novapalavra), palavratemp, len(palavratemp))
+    palavratemp = letraescolhida(letra, palavraescolhida, novapalavra)
     if novapalavra != palavratemp:
         novapalavra = palavratemp
         print('A palavra é: ' + novapalavra)
     else:
-        print(f'Você errou pela {erro:.0f}ª vez. Tente de novo!')
+        print(f'Você errou pela {erro + 1:.0f}ª vez. Tente de novo!')
         erro += 1
     print()
+    if novapalavra == palavraescolhida:
+        print('Parabéns, você acertou a palavra!')
+        break
+    elif erro == 6:
+        print('Que pena! Você foi enforcado')
+        break
 input()
